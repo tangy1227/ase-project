b15a94c9d1cbafed2abe1898c898b2a22c46bc63
tangy1227 <owentang1227@gmail.com>
2024-04-04 14:42:04 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
-use std::fs::File;
-use std::io::{self, Read};
+use sofar::reader::{OpenOptions, Filter};
+use sofar::render::Renderer;
 
-fn main() -> io::Result<()> {
-    // Open the file in read-only mode
-    let mut file = File::open("/Users/Owen/Documents/GitHub/ase-project/SOFA-data/012.sofa")?;
+fn main() {
+    println!("hello world");
 
-    // Read the contents of the file into a buffer
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer)?;
+    // Open sofa file, resample HRTF data if needed to 44100
+    let sofa = OpenOptions::new()
+        .sample_rate(44100.0)
+        .open("/Users/Owen/Documents/GitHub/ase-project/SOFA-data/HRIR_FULL2DEG.sofa")
+        .unwrap();
 
-    // Print the contents of the file
-    println!("{}", buffer);
+    let filt_len = sofa.filter_len();
+    let mut filter = Filter::new(filt_len);
 
-    Ok(())
+    // Get filter at poistion
+    sofa.filter(0.0, 1.0, 0.0, &mut filter);
+
+    let mut render = Renderer::builder(filt_len)
+        .with_sample_rate(44100.0)
+        .with_partition_len(64)
+        .build()
+        .unwrap();
+
+    let filt_len = sofa.filter_len();
+    let mut filter = Filter::new(filt_len);
+    
+    // Get filter at poistion
+    sofa.filter(0.0, 1.0, 0.0, &mut filter);
+    
+    let mut render = Renderer::builder(filt_len)
+        .with_sample_rate(44100.0)
+        .with_partition_len(64)
+        .build()
+        .unwrap();
+    
+    render.set_filter(&filter);
+    
+    let input = vec![0.0; 256];
+    let mut left = vec![0.0; 256];
+    let mut right = vec![0.0; 256];
+    
+    // read_input()
+    
+    render.process_block(&input, &mut left, &mut right).unwrap();
+
 }
+
